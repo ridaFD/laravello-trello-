9533ecf03d4deaa0cd6af3ebd03f899009bc1ae0
Rida fd <ridafakherlden@gmail.com>
2021-02-03 04:44:34 +0200

    Authentication frontend setup - CSRF Token & sending cookies

--- public/js/app.js
+++ public/js/app.js
@@ -8124,7 +8124,11 @@ __webpack_require__.r(__webpack_exports__);
 
 vue__WEBPACK_IMPORTED_MODULE_1__.default.use(vue_apollo__WEBPACK_IMPORTED_MODULE_0__.default);
 var apolloClient = new apollo_boost__WEBPACK_IMPORTED_MODULE_2__.default({
-  uri: 'http://laravello.test/graphql'
+  uri: 'http://laravello.test/graphql',
+  headers: {
+    'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').content
+  },
+  credentials: 'include'
 });
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (new vue_apollo__WEBPACK_IMPORTED_MODULE_0__.default({
   defaultClient: apolloClient
--- resources/js/apollo.config.js
+++ resources/js/apollo.config.js
@@ -5,7 +5,11 @@ import VueApollo from 'vue-apollo';
 Vue.use(VueApollo);
 
 const apolloClient = new ApolloClient({
-    uri: 'http://laravello.test/graphql'
+    uri: 'http://laravello.test/graphql',
+    headers: {
+        'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').content
+    },
+    credentials: 'include'
 });
 
 export default new VueApollo({
